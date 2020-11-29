ca4ef56ff45a17d23de51937d2a07f3fd298388f
MinMin <lenhattan.1997@gmail.com>
2020-11-29 22:36:59 +0700

    carousel

---
+++ elearning/js/main.js
@@ -0,0 +1,22 @@
+
+window.addEventListener("load", function() {
+    truncateCardTitle();
+});
+
+
+function truncateCardTitle() {
+    var cardList = document.getElementsByClassName("card-title");
+    for(var i = 0; i < cardList.length; i++) {
+        var text = cardList[i].innerHTML;
+        var newText = truncateString(text, 50);
+        cardList[i].innerHTML = newText;
+    }
+}
+
+function truncateString(str, num) {
+    if(str.length > num) {
+        return str.slice(0, num) + "...";
+    } else {
+        return str;
+    }
+}
